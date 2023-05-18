a9a67e2a5637e21cfbff9c65d53e55bd6f9c8c01
Madsen <Chris.Madsen@gov.bc.ca>
2023-05-18 16:30:51 -0700

    almost working, leaflet not adding hexagons

--- FisherApp.R
+++ FisherApp.R
@@ -1,18 +1,19 @@
 library(shiny)
-library(adehabitatHS)
+# library(adehabitatHS)
 library(sf)
 library(bslib)
 library(tidyverse)
 library(leaflet)
+library(data.table)
 library(Ryacas) # for the TeXForm command
 
 
 source('modules/habitat_slider_module.R')
 
 # Habitat variable names as vector.
-habitat_varnames = tibble(real_name = c('den_perc','branch_perc',
-                                        'cwd_perc','cav_perc',
-                                        'active_perc','open_perc'),
+habitat_varnames = tibble(real_name = c('den','branch',
+                                        'cwd','cav',
+                                        'active','open'),
                           label_name = c('Denning','Branch Resting',
                                          'CWD Resting','Cavity Resting',
                                          'Active','Open (less than)'))
@@ -72,12 +73,12 @@ spat_ret_sidebar = sidebar(
             accept = c(".zip",".gpkg")),
   selectInput(inputId = 'variable_for_leaflet',
               label = 'Habitat Type to Visualize',
-              choices = c('den_perc' = 'Denning',
-                          'branch_perc' = 'Branch Resting',
-                          'cwd_perc' = 'CWD Resting',
-                          'cav_perc' = 'Cavity Resting',
-                          'active_perc' = 'Active',
-                          'open_perc' = 'Open (less than)'),
+              choices = c('Denning' = 'den',
+                          'Branch Resting' = 'branch',
+                          'CWD Resting' = 'cwd',
+                          'Cavity Resting' = 'cav',
+                          'Active' = 'active',
+                          'Open (less than)' = 'open'),
               selectize = F
               )
 )
@@ -86,7 +87,8 @@ spat_ret_nav = nav(title = 'Spatial Retention Tool',
     layout_sidebar(
       sidebar = spat_ret_sidebar,
       card(
-        leafletOutput('myleaf')
+        leafletOutput('myleaf', height = '700px')#,
+        # DT::DTOutput('data_test')
       )
     ))
 
@@ -110,11 +112,24 @@ ui <- bslib::page_navbar(
 
 server <- function(input, output, session) {
 
-  if(!str_detect(getwd(),'www$')) setwd(paste0(getwd(),'/www/'))
+  # if(!str_detect(getwd(),'www$')) setwd(paste0(getwd(),'/www/'))
 
-  # Load in the fake hex data.
-  hex_data = read_sf('fake_hex_data.gpkg') |>
+  # Load in generated data.
+  bc = bcmaps::bc_bound() |> st_transform(crs = 4326)
+
+  subb_moist = read_sf('data/sub_boreal_moist.gpkg') |>
     st_transform(crs = 4326)
+
+  tsa_polys = read_sf('data/tsa_polygons_in_zone_simplfied.gpkg') |>
+    st_transform(crs = 4326)
+
+  hex_data = read_sf('data/hexagons_subb_moist.gpkg') |>
+    st_transform(crs = 4326)
+
+  subb_moist_pixels = reactive({
+    read_sf('data/subb_moist_pixels.gpkg') |>
+    st_transform(crs = 4326)
+  })
 
   # Make a label/numeric input/slider combo for each variable name.
   variable_values = lapply(habitat_varnames$real_name, \(x) habitat_slider_module_server(x, habitat_varnames))
@@ -155,39 +170,72 @@ server <- function(input, output, session) {
   # --------------------------------------------------------------
   # Nav 2: Habitat Retention Tool
 
+  # Summarise percentages for each hexagon.
+  hex_with_values = reactive({
+
+    subb_moist_pixels_dt = as.data.table(subb_moist_pixels() |> st_drop_geometry())
+
+    subb_moist_pixels_dt = subb_moist_pixels_dt[,lapply(.SD, \(x) sum(x)/.N), by = hex_ID, .SDcols = names(subb_moist_pixels_dt)[1:6]]
+
+    hex_data = hex_data |>
+      dplyr::select(hex_ID,X,Y,bec_zone) |>
+      left_join(as.data.frame(subb_moist_pixels_dt))
+
+    hex_data = st_set_geometry(hex_data, hex_data$geom)
+
+  })
+
   # Which variable to see on the leaflet map?
   chosen_variable = reactive({
     input$variable_for_leaflet
   })
 
   hex_data_var_chosen = reactive({
-    hex_data |>
-      mutate(chosen_variable = chosen_variable())
+    browser()
+    hex_with_values() |>
+      mutate(var_to_display = round(!!sym(chosen_variable()),3)) |>
+      dplyr::select(hex_ID,var_to_display)
   })
 
-  # my_fill_pal = reactive({
-  #   browser()
-  #   leaflet::colorNumeric(palette = 'RdYlGn',
-  #                                     domain = hex_data_var_chosen()$chosen_variable)
-  # })
-  #
-  # output$myleaf = renderLeaflet({
-  #
-  #   leaflet() |>
-  #     addProviderTiles(providers$CartoDB) |>
-  #     addPolygons(
-  #       fillColor = ~ my_fill_pal()(chosen_variable),
-  #       data = hex_data_var_chosen()
-  #     )
-  # })
-  #
-  # observe({
-  #   leafletProxy('myleaf') |>
-  #     addPolygons(
-  #       fillColor = ~my_fill_pal(chosen_variable),
-  #       data = hex_data_var_chosen()
-  #     )
-  # })
+  my_fill_pal = reactive({
+    leaflet::colorNumeric(palette = 'RdYlGn',
+                          domain = hex_data_var_chosen()$var_to_display)
+  })
+
+  output$myleaf = renderLeaflet({
+    leaflet() |>
+      addProviderTiles(providers$CartoDB) |>
+      addPolygons(
+        col = 'black',
+        weight = 1,
+        fillColor = 'transparent',
+        data = bc) |>
+      addPolygons(
+        color = 'darkgreen',
+        weight = 2,
+        fillColor = 'darkgreen',
+        opacity = 0.5,
+        data = subb_moist
+      )
+      # addLegend()
+  })
+
+  observe({
+    leafletProxy('myleaf') |>
+      clearGroup(group = 'hexagons_with_values') |>
+      addPolygons(
+        fillColor = 'grey',#~ my_fill_pal()(var_to_display),
+        fillOpacity = 0.8,
+        col = 'black',
+        weight = 3,
+        data = hex_data_var_chosen(),
+        group = 'hexagons_with_values'
+      )
+  })
+
+  output$data_test = DT::renderDT({
+    hex_data_var_chosen()
+  })
   # bc_grid = sf::st_as_sf(
   #   sf::st_make_grid(x = bcmaps::bc_bound() |> st_transform(crs = 4326),
   #                    n = c(10,10))
--- make_fake_data_rasters.R
+++ make_fake_data_rasters.R
@@ -2,13 +2,10 @@ library(bcmaps)
 library(sf)
 library(tidyverse)
 library(bcdata)
+library(leaflet)
 
-all_layers = bcdata::bcdc_list()
-all_layers[str_detect(all_layers,"biogeoclimat")]
-
-if(!file.exists('data/biogeoclimatic_zones_fisher.gpkg')){
-
-}
+# ------------------------------------------
+# Load and Filter Data!
 
 # Name of variables - this might need correcting.
 variable_name_df  = tibble(
@@ -23,26 +20,103 @@ bc = bcmaps::bc_bound() |>
   mutate(province = 'bc') |>
   dplyr::select(province)
 
+# Pull in BEC zones. We can use these to approximate data areas for now, until we get
+# data from Kyle.
+if(!file.exists('data/sub_boreal_moist.gpkg')){
 beczones = bcmaps::bec()
-# Make raster of 1 hectare size... very large.
-one_ha_pixels = st_make_grid(bc, cellsize = units::set_units(10000, "m2"))
 
-write_sf(one_ha_pixels, 'www/one_hectare_size_test.gpkg')
+# Filter BEC zones for just moist sub-boreal (for now!)
+# subb_dry = beczones |>
+#   filter(str_detect(ZONE_NAME,"Sub-Boreal") & str_detect(SUBZONE_NAME,'Dry'))
+
+subb_moist = beczones |>
+  filter(str_detect(ZONE_NAME,"Sub-Boreal") & str_detect(SUBZONE_NAME,'Moist'))
+
+# Summarise subb_moist to single polygon.
+subb_moist_s = subb_moist |>
+  summarise(bec_zone = 'sub-boreal moist')
+
+# Simplify the subb_moist shapefile... might be dangerous for data fidelity to do this!
+subb_moist_s = rmapshaper::ms_simplify(subb_moist_s)
+
+ggplot() +
+  geom_sf(data = bc) +
+  geom_sf(data = subb_moist_s, fill = 'lightblue')
+
+write_sf(subb_moist_s, 'data/sub_boreal_moist.gpkg')
+} else {subb_moist = read_sf('data/sub_boreal_moist.gpkg')}
+
+# Get TSAs
+if(!file.exists('data/all_tsa_polygons.gpkg')){
+
+  all_layers = bcdata::bcdc_list()
+
+  tsa_names = all_layers[str_detect(all_layers, 'fadm-timber-supply')]
+
+  tsa_polys = bcdc_query_geodata(tsa_names[1]) |>
+    collect()
+
+  write_sf(tsa_polys, 'data/all_tsa_polygons.gpkg')
+} else {tsa_polys = read_sf('data/all_tsa_polygons.gpkg')}
+
+if(!file.exists('data/tsa_polygons_in_zone_simplfied.gpkg')){
+# Remove TSAs that have been retired before today's date.
+tsa_polys = tsa_polys |> filter(is.na(RETIREMENT_DATE))
+
+# Remove TSAs that fall outside of our area of interest.
+tsa_polys = tsa_polys |>
+  st_join(subb_moist_s,
+          st_intersects) |>
+  filter(!is.na(bec_zone))
+
+ggplot() +
+  geom_sf(data = subb_moist_s, fill = 'brown') +
+  geom_sf(data = tsa_polys, fill = 'lightblue', alpha = 0.5)
+
+# Simplify the geometries of the TSAs a little bit... this might not be okay to do!
+tsa_polys = rmapshaper::ms_simplify(tsa_polys)
+
+tsa_polys = st_transform(tsa_polys, crs = 4326)
+
+tsa_polys = tsa_polys |>
+  mutate(map_label = coalesce(TSB_NUMBER_DESCRIPTION,TSA_NUMBER_DESCRIPTION))
+
+write_sf(tsa_polys, 'data/tsa_polygons_in_zone_simplfied.gpkg')
+} else {tsa_polys = read_sf('data/tsa_polygons_in_zone_simplfied.gpkg')}
+
+leaflet() |>
+  addTiles() |>
+  addPolygons(
+    label = ~map_label,
+    color = 'black',
+    weight = 2,
+    fillOpacity = 0.5,
+    fillColor = 'darkgreen',
+    data = tsa_polys
+  )
+
+# Example cutblocks
+cutblock = read_sf('data/A20001_274_blocks.shp') |>
+  st_transform(crs = 3005)
+
+# Make raster of 1 hectare size over the moist sub-boreal area.
+# TOO BIG #
+# one_ha_pixels = st_make_grid(subb_moist, cellsize = units::set_units(100000, "m2"))
+
+# one_ha_points = st_centroid(one_ha_pixels)
+
+# write_sf(one_ha_pixels, 'www/one_hectare_size_test.gpkg')
 
 # NOTE: I will need to change the size to 10 times smaller (i.e. 3e07)
 #       for production.
-hexagon_grid = st_make_grid(bc, square = F, cellsize = units::set_units(3e08, "m2"))
+if(!file.exists('data/hexagons_subb_moist.gpkg')){
+hexagon_grid = st_make_grid(subb_moist_s, square = F, cellsize = units::set_units(3e07, "m2"))
 
 hexagon_grid = st_set_geometry(tibble(placeholder = rep(1,length(hexagon_grid))), hexagon_grid)
 
-# Trim for BC.
-hexagon_grid = st_join(hexagon_grid, bc) |>
-  filter(!is.na(province)) |>
-  dplyr::select(-province)
-
-ggplot() +
-  geom_sf(data = hexagon_grid, aes(fill = placeholder)) +
-  scale_fill_fermenter(palette = 'RdYlGn')
+# ggplot() +
+#   geom_sf(data = hexagon_grid, aes(fill = placeholder)) +
+#   scale_fill_fermenter(palette = 'RdYlGn')
 
 # Add in an ID column for the hexagons.
 hexagon_grid$ID = row_number(hexagon_grid)
@@ -54,21 +128,8 @@ hexagon_grid = hexagon_grid |>
   arrange(Y,X) |>
   mutate(ID = row_number())
 
-ggplot() +
-  geom_sf(data = hexagon_grid, aes(fill = ID))
-
-# # Make values a bit more similar, based on nearest neighbours?
-# hex_averaged = hexagon_grid |>
-#   mutate(averaged_value = case_when(
-#     sample(c(0,1),nrow(hexagon_grid),replace = T) == 1 ~ (value + lead(value) + lag(value))/3,
-#     T ~ value)) |>
-#   mutate(averaged_value = ifelse(is.na(averaged_value),value,averaged_value))
-#
 # ggplot() +
-#   geom_sf(data = hex_averaged, aes(fill = averaged_value)) +
-#   scale_fill_fermenter(palette = 'RdYlGn', direction = 1)
-
-# hex_averaged
+#   geom_sf(data = hexagon_grid, aes(fill = ID))
 
 make_fake_data = function(dat, var_name){
 
@@ -77,7 +138,7 @@ make_fake_data = function(dat, var_name){
   # Make values a bit more similar, based on nearest neighbours?
   dat |>
     mutate(!!sym(var_name) := case_when(
-      sample(c(0,1),nrow(hexagon_grid),replace = T) == 1 ~ (base_values + lead(base_values) + lag(base_values))/3,
+      sample(c(0,1),nrow(dat),replace = T) == 1 ~ (base_values + lead(base_values) + lag(base_values))/3,
       T ~ base_values)) |>
     mutate(!!sym(var_name) := ifelse(is.na(!!sym(var_name)),
                                      base_values,
@@ -94,7 +155,77 @@ hexagon_grid = hexagon_grid |>
   make_fake_data(var_name = variable_name_df$real_name[6]) |>
   dplyr::select(-placeholder)
 
+# Trim for area of interest.
+hexagon_grid = hexagon_grid |>
+  st_join(subb_moist_s, st_intersects) |>
+  filter(!is.na(bec_zone))
+
+write_sf(hexagon_grid, 'data/hexagons_subb_moist.gpkg')
+} else {hexagon_grid = read_sf('data/hexagons_subb_moist.gpkg')}
+
 ggplot() +
   geom_sf(data = hexagon_grid, aes(fill = den_perc))
 
-write_sf(hexagon_grid, 'www/fake_hex_data.gpkg')
+# Make 100-hectare squares for our area of interest.
+if(!file.exists('data/subb_moist_pixels.gpkg')){
+subb_moist_pixels = st_make_grid(x = hexagon_grid, cellsize = units::set_units(1000000, "m2"))
+
+subb_moist_pixels = st_set_geometry(tibble(placeholder = rep(1,length(subb_moist_pixels))), subb_moist_pixels)
+
+# Just keep pixels that are inside the hexagons overlaying the BEC zone of interest.
+subb_moist_pixels = subb_moist_pixels |>
+  st_join(hexagon_grid |> dplyr::select(hex_ID)) |>
+  filter(!is.na(hex_ID))
+
+# Add in an ID column for the subb_moist_pixels
+subb_moist_pixels$X = st_coordinates(st_centroid(subb_moist_pixels))[,1]
+subb_moist_pixels$Y = st_coordinates(st_centroid(subb_moist_pixels))[,2]
+
+subb_moist_pixels = subb_moist_pixels |>
+  arrange(Y,X) |>
+  mutate(ID = row_number()) |>
+  dplyr::select(-X, -Y)
+
+subb_moist_pixels = subb_moist_pixels |>
+  mutate(den = sample(c(0,1),nrow(subb_moist_pixels), replace = T),
+         branch = sample(c(0,1),nrow(subb_moist_pixels), replace = T),
+         cwd = sample(c(0,1),nrow(subb_moist_pixels), replace = T),
+         cav = sample(c(0,1),nrow(subb_moist_pixels), replace = T),
+         active = sample(c(0,1),nrow(subb_moist_pixels), replace = T),
+         open = sample(c(0,1),nrow(subb_moist_pixels), replace = T),
+         )
+
+write_sf(subb_moist_pixels, 'data/subb_moist_pixels.gpkg')
+} else {subb_moist_pixels = read_sf('data/subb_moist_pixels.gpkg')}
+
+# Testing how to summarise for each hexagon.
+library(data.table)
+subb_moist_pixels_dt = as.data.table(subb_moist_pixels |> st_drop_geometry())
+
+subb_moist_pixels_dt = subb_moist_pixels_dt[,lapply(.SD, \(x) sum(x)/.N), by = hex_ID, .SDcols = names(subb_moist_pixels_dt)[1:6]]
+
+ggplot() + geom_sf(data = hexagon_grid, aes(fill = den_perc))
+
+hexagons_with_values = hexagon_grid |>
+  dplyr::select(hex_ID,X,Y,bec_zone) |>
+  left_join(subb_moist_pixels_dt)
+
+ggplot() + geom_sf(data = hexagons_with_values, aes(fill = den))
+
+## TESTING ##
+# How big are the cutblocks? How many 1-hectare squares would fit inside?
+# Let's make the 1-hectare squares to fill whichever hexagons fit onto these cutblocks.
+pixels_for_cutblock = st_make_grid(x = cutblock, cellsize = units::set_units(10000, "m2"))
+
+pixels_for_1_hex = st_make_grid(x = hex_for_cutblock[1],cellsize = units::set_units(10000, "m2"))
+pixels_for_cutblock = st_set_geometry(tibble(placeholder = abs(rnorm(length(pixels_for_cutblock),
+                                                                 mean = 0.5,
+                                                                 sd = 0.1))), pixels_for_cutblock)
+
+hex_for_cutblock = hexagon_grid = st_make_grid(cutblock, square = F, cellsize = units::set_units(3e07, "m2"))
+
+ggplot() +
+  geom_sf(data = pixels_for_cutblock, aes(fill = placeholder)) +
+  geom_sf(data = cutblock, alpha = 0.6, fill = 'red') +
+  geom_sf(data = hex_for_cutblock, col = 'blue', alpha = 0.5, fill = 'transparent') +
+  scale_fill_distiller(palette = 'RdYlGn', direction = 1)
